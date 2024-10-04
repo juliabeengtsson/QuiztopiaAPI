be05083090c934c06e701bbbf5489100ed650059
juliabengtsson2 <julia.bengtsson2@hotmail.com>
2024-10-04 11:10:39 +0200

    createAccount function done

--- src/createAccount/index.js
+++ src/createAccount/index.js
@@ -1,5 +1,56 @@
+//const AWS = require('aws-sdk');
+const { dynamoDb } = require('../../services/database');
+const { sendError, sendResponse } = require('../../services/response');
+const bcrypt = require('bcryptjs');
+const { v4: uuidv4 } = require('uuid');
+const { QueryCommand, PutCommand } = require('@aws-sdk/lib-dynamodb');
 
+exports.handler = async (event) => {
+    try {
+        const { username, password } = JSON.parse(event.body);
 
-exports.handler = async() => {
-    console.log('hello girl!')
+        if(!username || !password) {
+            return sendResponse(400, 'Username and password are required')
+        }
+
+        // Kollar ifall användarnamnet redan finns i databasen
+        const checkParams = {
+            TableName: process.env.USERS_TABLE,
+            IndexName: 'UsernameIndex',
+            KeyConditionExpression: 'username = :username',
+            ExpressionAttributeValues: {
+                ':username': username,
+            },
+        };
+
+        const command = new QueryCommand(checkParams);
+        const result = await dynamoDb.send(command);
+
+        if (result.Items.length > 0) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: 'Username already exists' }),
+            };
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+        const userId = uuidv4();
+
+        const params = {
+            TableName: process.env.USERS_TABLE,
+            Item: {
+              userId,
+              username,
+              hashPassword,
+            }
+        };
+
+        const putCommand = new PutCommand(params);
+        await dynamoDb.send(putCommand);
+
+        return sendResponse(201, {message: 'User created', userId})
+ 
+    } catch(error) {
+        return sendError(500, error);
+    }
 };
